d30c39c22a69e0755bf72c2c45039efd8f06a061
Cayden Pierce <cpierc3@uwo.ca>
2021-07-09 00:13:40 -0600

    cleaned code to be modular

--- README.md
+++ README.md
@@ -13,3 +13,11 @@
     -coqui tts works great but can we get it running in a script real time on the GPU?
     -other faster option?
     
+# SETUP
+
+1. Clone this repo
+2. Make a virtualenv
+3. Install requirements (`pip3 install -r requirements.txt`)
+4. Make file `./wolfram_api_key.txt` and paste your WolframOne App Id in there
+5. Setup GCP to allow Speech to Text, make credentials for it, and place your JSON credentials at `./utils/creds.json`
+6. Run `main.py`
--- main.py
+++ main.py
@@ -8,34 +8,15 @@ Right now just shoving everything into this file and will modularize as the prog
 @author: Cayden Pierce
 Based on the google speech file provided by Google
 """
-
-#Copyright for the Google STT streaming code
-#https://github.com/googleapis/python-speech/blob/master/samples/microphone/transcribe_streaming_infinite.py
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import requests
 import urllib
 from playsound import playsound
 import subprocess
 from fuzzysearch import find_near_matches
-import re
 import sys
 import time
-from google.cloud import speech
-import pyaudio
-from six.moves import queue
+
+from utils.gcp_stt import run_google_stt
 
 #config files
 wake_words_file = "./wakewords.txt"
@@ -47,12 +28,8 @@ command_success_sound = "./speech_pre_rendered/command_success.wav"
 generic_failure_sound = "./speech_pre_rendered/command_failed.wav"
 wolfram_failure_sound = "./speech_pre_rendered/wolfram_query_failed.wav"
 
-#set gcloud API key
-import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=os.path.join(os.path.dirname(__file__), "creds.json")
-
 # Audio recording parameters
-STREAMING_LIMIT = 240000 * 10 # 40 minutes
+STREAMING_LIMIT = 240000 # 4 minutes
 SAMPLE_RATE = 16000
 CHUNK_SIZE = int(SAMPLE_RATE / 10)  # 100ms
 
@@ -133,7 +110,7 @@ voice_commands = {
         "stop listening" : {"function" : None, "voice_sounds" : ["stop listening", "go deaf"]}, #end the program loop running (voice rec continues to run)
         "shell" : {"function" : None, "voice_sounds" : ["CLI", "shell", "bash", "zee shell", "zsh", "z shell"]}, #pass command directly to the terminal window we opened #TODO implement this with python `cli` package
         "add wake word" : {"function" : add_wake_word, "voice_sounds" : ["add wake word", "new wake word"]}, #pass command directly to the terminal window we opened #TODO implement this with python `cli` package
-        "save memory" : {"function" : save_memory, "voice_sounds" : ["save memory", "save speech", "mxt cache", "mxt remember"]}, #pass command directly to the terminal window we opened #TODO implement this with python `cli` package
+        "save memory" : {"function" : save_memory, "voice_sounds" : ["save memory", "save speech", "mxt cache", "mxt remember", "remember speech"]}, #pass command directly to the terminal window we opened #TODO implement this with python `cli` package
         "ask wolfram" : {"function" : ask_wolfram, "fail_function" : wolfram_failed, "voice_sounds" : ["Wolfram", "Wolfram Alpha", "ask Wolfram"]}, #pass command directly to the terminal window we opened #TODO implement this with python `cli` package
         "go to"  : {"function" : None, "voice_sounds" : ["select", "choose", "go to"]}, #start a new program mode (enter different mental upgrade loop, or start a 'suite' of mental upgrades, i.e. "go to social mode"
         }
@@ -233,123 +210,7 @@ def get_current_time():
     return int(round(time.time() * 1000))
 
 
-class ResumableMicrophoneStream:
-    """Opens a recording stream as a generator yielding the audio chunks."""
-
-    def __init__(self, rate, chunk_size):
-        self._rate = rate
-        self.deaf = False #if set to true, we go deaf to incoming data
-        self.chunk_size = chunk_size
-        self._num_channels = 1
-        self._buff = queue.Queue()
-        self.closed = True
-        self.start_time = get_current_time()
-        self.restart_counter = 0
-        self.audio_input = []
-        self.last_audio_input = []
-        self.result_end_time = 0
-        self.is_final_end_time = 0
-        self.final_request_end_time = 0
-        self.bridging_offset = 0
-        self.last_transcript_was_final = False
-        self.new_stream = True
-        self._audio_interface = pyaudio.PyAudio()
-        self._audio_stream = self._audio_interface.open(
-            format=pyaudio.paInt16,
-            channels=self._num_channels,
-            rate=self._rate,
-            input=True,
-            frames_per_buffer=self.chunk_size,
-            # Run the audio stream asynchronously to fill the buffer object.
-            # This is necessary so that the input device's buffer doesn't
-            # overflow while the calling thread makes network requests, etc.
-            stream_callback=self._fill_buffer,
-        )
-
-    def __enter__(self):
-
-        self.closed = False
-        return self
-
-    def __exit__(self, type, value, traceback):
-
-        self._audio_stream.stop_stream()
-        self._audio_stream.close()
-        self.closed = True
-        # Signal the generator to terminate so that the client's
-        # streaming_recognize method will not block the process termination.
-        self._buff.put(None)
-        self._audio_interface.terminate()
-
-    def _fill_buffer(self, in_data, *args, **kwargs):
-        """Continuously collect data from the audio stream, into the buffer."""
-
-        #if we are deaf, save data as zeros
-        if not self.deaf:
-            self._buff.put(in_data)
-        else:
-            self._buff.put(bytes(len(in_data)))
-
-        return None, pyaudio.paContinue
-
-    def generator(self):
-        """Stream Audio from microphone to API and to local buffer"""
-
-        while not self.closed:
-            data = []
-
-            if self.new_stream and self.last_audio_input:
-
-                chunk_time = STREAMING_LIMIT / len(self.last_audio_input)
-
-                if chunk_time != 0:
-
-                    if self.bridging_offset < 0:
-                        self.bridging_offset = 0
-
-                    if self.bridging_offset > self.final_request_end_time:
-                        self.bridging_offset = self.final_request_end_time
-
-                    chunks_from_ms = round(
-                        (self.final_request_end_time - self.bridging_offset)
-                        / chunk_time
-                    )
-
-                    self.bridging_offset = round(
-                        (len(self.last_audio_input) - chunks_from_ms) * chunk_time
-                    )
-
-                    for i in range(chunks_from_ms, len(self.last_audio_input)):
-                        data.append(self.last_audio_input[i])
-
-                self.new_stream = False
-
-            # Use a blocking get() to ensure there's at least one chunk of
-            # data, and stop iteration if the chunk is None, indicating the
-            # end of the audio stream.
-            chunk = self._buff.get()
-            self.audio_input.append(chunk)
-
-            if chunk is None:
-                return
-            data.append(chunk)
-            # Now consume whatever other data's still buffered.
-            while True:
-                try:
-                    chunk = self._buff.get(block=False)
-
-                    if chunk is None:
-                        return
-                    data.append(chunk)
-                    self.audio_input.append(chunk)
-
-                except queue.Empty:
-                    break
-
-            yield b"".join(data)
-
-
-def listen_print_loop(responses, stream):
+def parse_transcriptions(responses, stream):
     """Iterates through server responses and prints them.
 
     The responses passed is a generator that will block until a response
@@ -419,59 +280,9 @@ def listen_print_loop(responses, stream):
 
 
 def main():
-    """start bidirectional streaming from microphone input to speech API"""
-
-    client = speech.SpeechClient()
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=SAMPLE_RATE,
-        language_code="en-US",
-        max_alternatives=1,
-        speech_contexts=[{"phrases" : ["Licklider", "mind expansion", "mind extension", "Cayden", "Cayden Pierce", "BCI", "wearables", "engineering", "bash", "shell"]}] #TODO make this pull from our wake words and voice commands list automatically
-    )
-
-    streaming_config = speech.StreamingRecognitionConfig(
-        config=config, interim_results=True
-    )
-
-    mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
-    print(mic_manager.chunk_size)
-    sys.stdout.write(YELLOW)
-    sys.stdout.write('\nListening, begin entering voice commands now.\n\n')
-    sys.stdout.write("End (ms)       Transcript Results/Status\n")
-    sys.stdout.write("=====================================================\n")
-
-    with mic_manager as stream:
-        while not stream.closed:
-            sys.stdout.write(YELLOW)
-            sys.stdout.write(
-                "\n" + str(STREAMING_LIMIT * stream.restart_counter) + ": NEW REQUEST\n"
-            )
-
-            stream.audio_input = []
-            audio_generator = stream.generator()
-
-            requests = (
-                speech.StreamingRecognizeRequest(audio_content=content)
-                for content in audio_generator
-            )
-
-            responses = client.streaming_recognize(streaming_config, requests)
-
-            # Now, put the transcription responses to use.
-            listen_print_loop(responses, stream)
-
-            if stream.result_end_time > 0:
-                stream.final_request_end_time = stream.is_final_end_time
-            stream.result_end_time = 0
-            stream.last_audio_input = []
-            stream.last_audio_input = stream.audio_input
-            stream.audio_input = []
-            stream.restart_counter = stream.restart_counter + 1
-
-            if not stream.last_transcript_was_final:
-                sys.stdout.write("\n")
-            stream.new_stream = True
+    #run speech to text, pass every result to the callback function passed in
+    #in the future if we use different STT libs, we can just change that right here
+    run_google_stt(parse_transcriptions)
 
 if __name__ == "__main__":
     main()
