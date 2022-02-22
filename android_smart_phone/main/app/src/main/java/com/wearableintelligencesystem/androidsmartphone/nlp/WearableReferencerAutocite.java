6bf95ac453366709d1d4bfa2b844fa81f51d8e49
Cayden Pierce <cpierc3@uwo.ca>
2022-02-22 15:05:39 -0500

    convo mode text size, autociter buffer fix
    Former-commit-id: e12dd4d49b82b588174895381a99d4b951d1fe87
    Former-commit-id: ae35b3dc7a4b758dae1f941561d0e261319c4f78
    Former-commit-id: 7833a479ee3157a6ac1eea6c7093b65de1ac36ff

--- android_smart_phone/main/app/src/main/java/com/wearableintelligencesystem/androidsmartphone/nlp/WearableReferencerAutocite.java
+++ android_smart_phone/main/app/src/main/java/com/wearableintelligencesystem/androidsmartphone/nlp/WearableReferencerAutocite.java
@@ -14,7 +14,6 @@ import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 
 import org.apache.commons.lang3.tuple.MutableTriple;
-import org.apache.commons.lang3.tuple.Triple;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
@@ -39,23 +38,26 @@ public class WearableReferencerAutocite {
     private List<String []> myReferences;
     private Context context;
 
+    private boolean processing = false;
+
     //selectors
     String [] selectorChars = new String[] {"α", "β", "γ", "Δ", "π"};
     String [] selectorCharNames = new String[] {"alpha", "beta", "gamma", "delta", "pie"};
-    List<Triple<Integer,Long, Integer>> potentialReferenceBuffer = new ArrayList<Triple<Integer,Long, Integer>>(); //stores all the references we sent out, so that know which one was selected (maps a reference to the selector we gave it)
-    private int potentialReferenceDecayTime = 45000; //amount of time before we disapear a reference
+    List<MutableTriple<Integer,Long, Integer>> potentialReferenceBuffer = new ArrayList<MutableTriple<Integer,Long, Integer>>(); // reference index, timestamp, selector index //stores all the references we sent out, so that know which one was selected (maps a reference to the selector we gave it)
+    private int potentialReferenceDecayTime = 20000; //amount of time before we disapear a reference
 
     private boolean awaitingSelection;
 
     //voice command fuzzy search threshold
-    private final double referenceThreshold = 0.92;
+    private final double referenceThreshold = 0.95;
+    private final double selectorCharNameThreshold = 0.90;
     private final int titleIndex = 1;
     private final int yearIndex = 2;
     private final int authorsIndex = 3;
     private final int linkIndex = 4;
     private final int keywordIndex = 5;
     private final int titleMinWordLen = 5;
-    private final int refCountThresh = 3; //how many reference hits before we suggest the reference
+    private final int refCountThresh = 4; //how many reference hits before we suggest the reference
 
     private NlpUtils nlpUtils;
 
@@ -81,10 +83,10 @@ public class WearableReferencerAutocite {
             CSVReader reader = new CSVReader(csvStreamReader);
             myReferences = reader.readAll();
             for (int i = 0; i < myReferences.size(); i++){
-                Log.d(TAG, "Printing references at index: " + i);
-                for (int j = 0; j < myReferences.get(i).length; j++){
-                    Log.d(TAG, "--- " + myReferences.get(i)[j]);
-                }
+//                Log.d(TAG, "Printing references at index: " + i);
+//                for (int j = 0; j < myReferences.get(i).length; j++){
+//                    Log.d(TAG, "--- " + myReferences.get(i)[j]);
+//                }
             }
         } catch (IOException e){
             Log.d(TAG, "openReferenecsFile failed:");
@@ -130,11 +132,14 @@ public class WearableReferencerAutocite {
                     //fuzzy match that with our possible selection
                     int selIdx = -1;
                     for (int i = 0; i < selectorCharNames.length; i++){
-                        String currSel = selectorCharNames[i];
-                       int find = nlpUtils.findNearMatches(selection, currSel, 0.80);
+                       String currSel = selectorCharNames[i];
+                       int find = nlpUtils.findNearMatches(selection, currSel, selectorCharNameThreshold);
                        if (find != -1){
                            if (i < potentialReferenceBuffer.size()) {
-                               sendReferenceToConversationPartner(myReferences.get(potentialReferenceBuffer.get(i).getLeft()));
+                               MutableTriple ref = getFromBuffer(i);
+                               if (ref != null) {
+                                   sendReferenceToConversationPartner(myReferences.get((int) ref.getLeft()));
+                               }
                            }
                            break;
                        }
@@ -143,7 +148,7 @@ public class WearableReferencerAutocite {
                     Log.d(TAG, "WearableReferencer got FINAL_TRANSCRIPT, parsing");
                     try{
                         String transcript = data.getString(MessageTypes.TRANSCRIPT_TEXT);
-                        parseString(transcript);
+                        handleTranscript(transcript);
                     } catch (JSONException e){
                         e.printStackTrace();
                     }
@@ -152,6 +157,22 @@ public class WearableReferencerAutocite {
         } catch (JSONException e){
             e.printStackTrace();
         }
+    }
+
+    private void handleTranscript(String transcript){
+        //setup a handler to process data
+        if (processing){
+            return;
+        }
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                processing = true;
+                parseString(transcript);
+                processing = false;
+            }
+        });
     }
 
     //searches incoming transcript to see if it contains any keywords
@@ -165,8 +186,7 @@ public class WearableReferencerAutocite {
             matchCount = matchCount + nlpUtils.findNumMatches(toReference, keywords, referenceThreshold);
             matchCount = matchCount + nlpUtils.findNumMatches(toReference, authors, referenceThreshold);
             matchCount = matchCount + nlpUtils.findNumMatches(toReference, title, referenceThreshold);
-            Log.d(TAG, "Got matchCount: " + matchCount + "; for index: " + i);
-            if (matchCount >= refCountThresh){
+            if (matchCount >= refCountThresh){ //if this is a match
                 //add to our buffer
                 addToBuffer(i);
             }
@@ -179,13 +199,24 @@ public class WearableReferencerAutocite {
         sendPotentialReferencesToAsg();
     }
 
-    private void addToBuffer(int refIdx){
+    private MutableTriple getFromBuffer(int selectionIdx){
+        for (int i = 0; i < potentialReferenceBuffer.size(); i++) {
+            MutableTriple currReference = potentialReferenceBuffer.get(i);
+            if (selectionIdx == (int) currReference.getRight()){
+                return currReference;
+            }
+        }
+        return null;
+    }
+
+        private void addToBuffer(int refIdx){
         long currTime = System.currentTimeMillis();
 
         //find an available selector index
         int selIdx = -1;
         for (int i = 0; i < selectorCharNames.length; i++) {
             boolean found = true;
+            //if we check every reference in the buffer and none of them have the selector i, then we can use that selector
             for (int j = 0; j < potentialReferenceBuffer.size(); j++) {
                 if (i == potentialReferenceBuffer.get(j).getRight()){
                     found = false;
@@ -198,11 +229,36 @@ public class WearableReferencerAutocite {
         }
 
         //there is no more room in the UI for more references, later this should pop oldest reference and replace it
+        int popIdx = -1;
         if (selIdx == -1){
-            return;
+            long oldestTime = System.currentTimeMillis() + 1000;
+            //go through, find oldest entry, and replace the oldest entry with the new reference we detected
+            for (int i = 0; i < potentialReferenceBuffer.size(); i++) {
+                long refTime = potentialReferenceBuffer.get(i).getMiddle();
+                if (refTime < oldestTime){
+                    oldestTime = refTime;
+                    selIdx = potentialReferenceBuffer.get(i).getRight();
+                    popIdx = i;
+                }
+            }
+            //we found the buffer index of the
+            potentialReferenceBuffer.remove(popIdx);
         }
 
-        potentialReferenceBuffer.add(new MutableTriple(refIdx, currTime, selIdx));
+        //before adding, check if the refIdx is already in the buffer
+        int alreadyShownIdx = -1;
+        for (int i = 0; i < potentialReferenceBuffer.size(); i++) {
+            if (refIdx == potentialReferenceBuffer.get(i).getLeft()){
+                alreadyShownIdx = i;
+                break;
+            }
+        }
+        //If it is already in the buffer, just update the time and leave it in there
+        if (alreadyShownIdx != -1){
+           potentialReferenceBuffer.get(alreadyShownIdx).setMiddle(System.currentTimeMillis());
+        } else {
+            potentialReferenceBuffer.add(new MutableTriple(refIdx, currTime, selIdx));
+        }
 
         //setup a handler to pop stale references after they expire
         handler.postDelayed(new Runnable() {
@@ -220,7 +276,7 @@ public class WearableReferencerAutocite {
         //use a toRemove list so we can remove things while iterating through the list - this is ugly and weird, maybe we should copy the list instead...?
         List<Integer> toRemove = new ArrayList<Integer>();
         for (int i = 0; i < potentialReferenceBuffer.size(); i++){
-            Triple<Integer, Long, Integer> refP = potentialReferenceBuffer.get(i);
+            MutableTriple<Integer, Long, Integer> refP = potentialReferenceBuffer.get(i);
             if ((refP.getMiddle() + potentialReferenceDecayTime) < currTime){
                 toRemove.add(i);
             }
@@ -229,7 +285,7 @@ public class WearableReferencerAutocite {
         //actually remove them
         int count = 0;
         for (Integer i : toRemove){
-            potentialReferenceBuffer.remove(i + count);
+            potentialReferenceBuffer.remove(i - count);
             count++;
         }
 
