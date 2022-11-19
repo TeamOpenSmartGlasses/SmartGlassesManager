9472965c74c8b08a20f992f035ea09bf2f5b2e04
cayden <cpierc3@uwo.ca>
2022-11-19 15:23:41 +0000

    fix bugs and update deploy setup

--- gnu_linux_box/backend/api/SemanticSearchApi.py
+++ gnu_linux_box/backend/api/SemanticSearchApi.py
@@ -1,4 +1,5 @@
 from flask import render_template, make_response
+import time
 from flask_restful import Resource, reqparse, fields, marshal_with
 from db import Database
 from datetime import datetime
