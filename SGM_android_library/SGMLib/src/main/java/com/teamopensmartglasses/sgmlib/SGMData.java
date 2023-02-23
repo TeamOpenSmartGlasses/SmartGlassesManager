133c45c0b743d8c917665e16b227be868ab39d03
cayden <cpierc3@uwo.ca>
2023-02-22 20:51:41 -0800

    refactor to make SGMLib an android library that the ASP SGM references as a library

---
+++ SGM_android_library/SGMLib/src/main/java/com/teamopensmartglasses/sgmlib/SGMData.java
@@ -0,0 +1,9 @@
+package com.teamopensmartglasses.sgmlib;
+
+import java.util.ArrayList;
+
+public class SGMData {
+    public static final String SGMPkgName = "com.teamopensmartglasses.sgmlib";
+    public static SGMBroadcastSender sgmBroadcastSender;
+    public static ArrayList<TPACommand> commandsToRegister;
+}
---
+++ SGM_android_library/SGMLib/src/main/java/com/teamopensmartglasses/sgmlib/TPACommand.java
@@ -0,0 +1,16 @@
+package com.teamopensmartglasses.sgmlib;
+
+import java.util.ArrayList;
+
+public class TPACommand {
+    String commandName;
+    String commandDescription;
+    ArrayList<String> callPhrases;
+
+    TPACommand(String commandName, String commandDescription, ArrayList<String> callPhrases)
+    {
+        this.commandName = commandName;
+        this.commandDescription = commandDescription;
+        this.callPhrases = callPhrases;
+    }
+}
