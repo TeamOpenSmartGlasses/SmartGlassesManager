35418e385d5c8a9de5db93242ff59fa075d8c07d
Sameer Iyengar <sameer.m.iyengar@gmail.com>
2023-11-05 19:23:42 -0600

    Update Monocle.java

--- android_smart_phone/main/app/src/main/java/com/smartglassesmanager/androidsmartphone/supportedglasses/Monocle.java
+++ android_smart_phone/main/app/src/main/java/com/smartglassesmanager/androidsmartphone/supportedglasses/Monocle.java
@@ -1,1 +1,18 @@
+package com.smartglassesmanager.androidsmartphone.supportedglasses;
 
+// these glasses are pretty bad. There's no home button/gesture... the text/font size is all off android specs. Arms are big. The company has zero customer support. We might drop support for these - cayden
+public class Monocle extends SmartGlassesDevice {
+    public Monocle() {
+        deviceModelName = "Monocle;
+        deviceIconName = "Monocle";
+        anySupport = true;
+        fullSupport = false;
+        glassesOs = SmartGlassesOperatingSystem.ANDROID_OS_GLASSES;
+        hasDisplay = true;
+        hasSpeakers = false;
+        hasCamera = true;
+        hasInMic = true;
+        hasOutMic = false; //unknown
+        weight = 76;
+    }
+}
