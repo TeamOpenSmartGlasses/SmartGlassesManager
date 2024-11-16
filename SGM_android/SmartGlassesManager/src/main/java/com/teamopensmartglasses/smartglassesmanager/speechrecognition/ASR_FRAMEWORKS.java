03ac3a0303a03cd3f8a0768647654ff9682097ac
cayden <cpierc3@uwo.ca>
2024-11-16 18:10:29 -0500

    remove Vosk and NDK filters to support BLU C5L Max

--- SGM_android/SmartGlassesManager/src/main/java/com/teamopensmartglasses/smartglassesmanager/speechrecognition/ASR_FRAMEWORKS.java
+++ SGM_android/SmartGlassesManager/src/main/java/com/teamopensmartglasses/smartglassesmanager/speechrecognition/ASR_FRAMEWORKS.java
@@ -1,7 +1,6 @@
 package com.teamopensmartglasses.smartglassesmanager.speechrecognition;
 
 public enum ASR_FRAMEWORKS {
-    VOSK_ASR_FRAMEWORK,
     GOOGLE_ASR_FRAMEWORK,
     DEEPGRAM_ASR_FRAMEWORK,
     AZURE_ASR_FRAMEWORK
